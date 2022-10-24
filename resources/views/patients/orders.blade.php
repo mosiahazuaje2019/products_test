042a007876ec5167828feca8c1bcae3c49428b89
mosiahazuaje2019 <mazuaje@petroalianza.com>
2022-10-24 10:47:16 -0500

    quitando general invoice

--- resources/views/patients/orders.blade.php
+++ resources/views/patients/orders.blade.php
@@ -125,12 +125,14 @@
                             $order->toArray(),
                             function ($sum, $patient) {
                                 $total_invoice = (float) ($sum += (float) $patient['product']['price']*$patient['prescription']);
-                                $general_invoice = $patient['order']['discount_percent'] > 0 ? round($total_invoice-($total_invoice*($patient['order']['discount_percent']/100)),2) : $total_invoice;
+                                //$general_invoice = $patient['order']['discount_percent'] > 0 ? round($total_invoice-($total_invoice*($patient['order']['discount_percent']/100)),2) : $total_invoice;
                                 return $general_invoice;
                             },
                             0,
                         );
-                    })
+                    })->reduce(function($a, $b){
+                            return $a+$b;
+                        })
                 }}
                 </strong>
             </td>
