fe80281ad0984195f6d3c5e79cbb673c7da052af
mosiahazuaje2019 <mazuaje@petroalianza.com>
2022-10-24 10:26:05 -0500

    cambio en distribucion de parentesis para la multiplicacion del total

--- resources/views/patients/orders.blade.php
+++ resources/views/patients/orders.blade.php
@@ -125,7 +125,7 @@
                             $order->toArray(),
                             function ($sum, $patient) {
                                 $total_invoice = (float) ($sum += (float) $patient['product']['price']*$patient['prescription']);
-                                $general_invoice = $patient['order']['discount_percent'] > 0 ? round(($total_invoice * $patient['order']['discount_percent']),2) : $total_invoice;
+                                $general_invoice = $patient['order']['discount_percent'] > 0 ? round(($total_invoice*$patient['order']['discount_percent']),2) : $total_invoice;
                                 return $general_invoice;
                             },
                             0,
@@ -133,7 +133,7 @@
                     })->reduce(function($a, $b){
                             return $a+$b;
                         })
-                }}
+                }}
                 </strong>
             </td>
 
